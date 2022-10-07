b25af1d7ab46a548d4813bf6682a7e5f6a9b00cd
javedkhan2724 <javedkhan2792001@gmail.com>
2022-10-07 12:40:19 +0530

    Create LMS

---
+++ LMS/src/com/infinite/lms/LeaveException.java
@@ -0,0 +1,8 @@
+package com.infinite.lms;
+
+public class LeaveException extends Exception{
+	public LeaveException(String error) {
+		// TODO Auto-generated constructor stub
+		super(error);
+	}
+}
